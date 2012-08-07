27a083b20601bcc634b85df38a732bd78a91210f
river <riverzhou2000@gmail.com>
2012-08-08 07:01:10 +0800

    fix path

--- get.sh
+++ get.sh
@@ -17,8 +17,8 @@ git checkout -f
 git apply ../local/bionic_libm_pow.patch
 cd ..
 
-cd frameworks
+cd frameworks/base
 git checkout -f
-git apply ../local/frameworks-base_rotation.patch
-cd ..
+git apply ../../local/frameworks-base_rotation.patch
+cd ../..
 
