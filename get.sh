e3628ca0b8d8695e88b7358d3458e66344d406d0
river <riverzhou2000@gmail.com>
2012-08-29 04:40:33 +0800

    add a dalvik patch

--- get.sh
+++ get.sh
@@ -19,4 +19,9 @@ git apply ../local/bionic_libm_pow.patch
 git apply ../local/bionic_linaro_str.patch
 cd ..
 
+cd dalvik
+git checkout -f
+git clean -d -f
+git apply ../local/dalvik_gc_bugfix.patch
+cd ..
 
