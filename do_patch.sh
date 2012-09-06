17d02ad4dda91d4a515e534720b19d091f8a9abe
river <riverzhou2000@gmail.com>
2012-09-06 11:50:44 +0800

    update do_patch

--- do_patch.sh
+++ do_patch.sh
@@ -3,13 +3,8 @@
 cd bionic
 git checkout -f
 git clean -d -f
+git pull github jellybean
 git apply ../local/bionic_libm_pow.patch
 git apply ../local/bionic_linaro_str.patch
 cd ..
-
-cd frameworks/av/
-git checkout -f
-git clean -d -f
-git apply ../../local/frame_av_omap3.patch
-cd ../..
 
--- get.sh
+++ get.sh
@@ -20,9 +20,3 @@ git apply ../local/bionic_libm_pow.patch
 git apply ../local/bionic_linaro_str.patch
 cd ..
 
-#cd frameworks/av/
-#git checkout -f
-#git clean -d -f
-#git apply ../../local/frame_av_omap3.patch
-#cd ../..
-
