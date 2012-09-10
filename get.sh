c9f17dfec5ee194f5fcc1da09bdce9dbd80bf630
River Zhou <riverzhou2000@gmail.com>
2012-09-10 21:19:46 +0800

    update patchs . add a script

--- get.sh
+++ get.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-rm prebuilts/gcc/linux-x86/arm/arm-eabi-4.6
-rm prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6
-
 repo sync -j16
-
-cp gcc prebuilts/tools/gcc-sdk/
-cp set.sh prebuilts/gcc/linux-x86/arm/
-
-cd prebuilts/gcc/linux-x86/arm
-./set.sh
-cd ../../../../
 
 cd bionic
 git checkout -f
